3e52677ccea4178020aa2fe9e9f496d47d0e3af4
root <aaroniniguez1@gmail.com>
2017-03-16 00:28:36 +0000

    added comments

--- writeToLCD.py
+++ writeToLCD.py
@@ -1,30 +1,8 @@
 #!/usr/bin/python
-#
-#
-# References:
-# http://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-of-eth0-in-python
 
-# https://github.com/intel-iot-devkit/upm/blob/master/examples/python/rgb-lcd.py
-
-# Permission is hereby granted, free of charge, to any person obtaining
-
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#run like this:
+#python writeToLCD "your message" 10
+#where 10 is in seconds how long you want the message to stay on the screen
 
 import sys
 import socket
